7d9f55c002dad6a2c79dc264f7fb295d04e83e33
Neil Kakkar <neilkakkar@192.168.1.103>
2021-05-04 16:13:42 +0100

    setup downsampling plugin

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
-# PostHog Plugin Advanced Kit: Hello World
+# Downsampling Plugin
 
-[![npm package](https://img.shields.io/npm/v/posthog-plugin-hello-world?style=flat-square)](https://www.npmjs.com/package/posthog-plugin-hello-world)
-[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg?style=flat-square)](https://opensource.org/licenses/MIT)
-
-This is an exemplary PostHog plugin. It adds property `"greeting"` to every event, with a configurable value (by default: `"Hello world!"`).
-
-Use it as a base for your own plugins!
-
-## Goodies included
-
--   TypeScript for best reliability and development experience
--   Jest for convenient testing
--   Prettier and ESLint for code style and best practices – both handled by pre-commit hooks
--   GitHub Actions set up to run code quality and functionality tests in pull requests, and also to publish a new npm release when the package version is incremented in `main` (the latter requires an automation type npm access token to be set with a repo secret `NPM_TOKEN`)
-
-Want to simply get started, without the above bloat? [PostHog Plugin Starter Kit](https://github.com/PostHog/posthog-plugin-starter-kit) should be just the right fit for you – only the essentials needed to get off the ground.
+Reduces the number of events going into PostHog
 
 ## Installation
 
--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -5,56 +5,24 @@ const { processEvent } = require('..')
 beforeEach(() => {
     resetMeta({
         config: {
-            greeting: 'Dzień dobry!',
+            percentage: '100',
         },
     })
 })
 
-test('processEvent adds properties', async () => {
+test('processEvent filters event', async () => {
     // create a random event
-    const event0 = createEvent({ event: 'booking completed', properties: { amount: '20', currency: 'USD' } })
+    const event0 = createEvent({ event: 'blah' })
 
-    // must clone the event since `processEvent` will mutate it otherwise
-    const event1 = await processEvent(clone(event0), getMeta())
-    expect(event1).toEqual({
-        ...event0,
-        properties: {
-            ...event0.properties,
-            greeting: 'Dzień dobry!',
-            greeting_counter: 0,
-            random_number: 4,
-        },
-    })
+    // TODO: Figure out how getMeta can manipulate meta
 
-    const event2 = await processEvent(clone(event0), getMeta())
-    expect(event2).toEqual({
-        ...event1,
-        properties: {
-            ...event1.properties,
-            greeting_counter: 1,
-        },
-    })
-
-    const event3 = await processEvent(clone(event0), getMeta())
-    expect(event3).toEqual({
-        ...event2,
-        properties: {
-            ...event2.properties,
-            greeting_counter: 2,
-        },
-    })
+    for(let i=0; i < 100; i++) {
+        const event1 = await processEvent(event0, getMeta())
+        expect(event1).toEqual(event0)
+    }
 })
 
-test('processEvent does not crash with identify', async () => {
-    const defaultHelloWorldProperties = Object.freeze({
-        greeting: 'Dzień dobry!',
-        greeting_counter: 0,
-        random_number: 4,
-    })
-    // create a random event
-    const event0 = createIdentify()
-    // must clone the event since `processEvent` will mutate it otherwise
-    const { properties, ...restOfEvent1 } = await processEvent(clone(event0), getMeta())
-    expect(restOfEvent1).toEqual(event0)
-    expect(properties).toEqual(defaultHelloWorldProperties)
+
+test('processEvent filters 0 events at 0 percent', async () => {
+    // TODO
 })
