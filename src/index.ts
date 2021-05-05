681ff2e023785fd6431e640f3f70e143221d5771
Neil Kakkar <neilkakkar@gmail.com>
2021-05-05 14:45:59 +0100

    add tests and address #2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { PluginEvent, PluginMeta } from '@posthog/plugin-scaffold'
+import { createHash } from 'crypto'
 
 export function setupPlugin({ config, global }: PluginMeta) {
-
     const percentage = parseFloat(config.percentage)
-    if ( isNaN(percentage) || percentage > 100 || percentage < 0) {
+    if (isNaN(percentage) || percentage > 100 || percentage < 0) {
         throw new Error('Percentage must be a number between 0 and 100.')
     }
     global.percentage = percentage
@@ -12,7 +12,18 @@ export function setupPlugin({ config, global }: PluginMeta) {
 // /* Runs on every event */
 export function processEvent(event: PluginEvent, { global }: PluginMeta) {
 
-    if (Math.random() < global.percentage / 100) {
+    // hash is a sha256 hash of the distinct_id represented in base 16
+    // We take the first 16 digits, convert this into an integer, 
+    // dividing by the biggest base 16 number to get a value between 0 and 1.
+    // This is stable, so a distinct_id that was allowed before will continue to be allowed,
+    // even if the percentage increases
+    const hash = createHash("sha256")
+        .update(event.distinct_id)
+        .digest("hex");
+    const decision_value = parseInt(hash.substring(0, 16), 16) / 0xffffffffffffffff;
+
+    console.log(decision_value)
+    if (decision_value < global.percentage / 100) {
         return event
     }
     return null
